c78e8ae53307493296b1023df3bfa5a6d3948be0
Ahmad <asalmuhtaseb@gmail.com>
2023-11-21 13:54:10 -0500

    Placeholder conditional to switch between exisiting algorithms

--- sorting_algorithms_project/Sorting Algorithms/main.cpp
+++ sorting_algorithms_project/Sorting Algorithms/main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 #include "tree_sort.h"
+void insertionSort(std::vector<int>& data)
+{
+    int i, key, j;
+    int n = data.size();
+    for (i = 1; i < n; i++) {
+        key = data[i];
+        j = i - 1;
+
+        while (j >= 0 && data[j] > key) {
+            data[j + 1] = data[j];
+            j = j - 1;
+        }
+        data[j + 1] = key;
+    }
+}
 
 void print(std::vector<int> &vec){
     for(int i = 0; i < vec.size(); i++) {
@@ -9,8 +24,21 @@ void print(std::vector<int> &vec){
 }
 
 int main() {
-    std::vector<int> vec = {7,67,68,84,94,12,13,82,420,632,932,931};
+    std::vector<int> vec = {7,67,68,84,94};
     TreeSort T;
-    T.sort(vec);
+    std::cout << "Unsorted: ";
     print(vec);
+    std::cout << std::endl;
+
+    if(vec.size() >= 5){
+        T.sort(vec);
+        print(vec);
+        std::cout << "Sorted with Tree Sort\n";
+    } else {
+        insertionSort(vec);
+        print(vec);
+        std::cout << "Sorted with Insertion Sort\n";
+    }
+
 }
+
